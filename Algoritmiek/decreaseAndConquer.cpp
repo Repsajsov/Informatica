ce7e16d7fd3e99dfa1128c766554ced145913c7c
Repsajsov <jaspervos12@outlook.com>
2026-01-13 00:10:33 +0100

    Implement binary search function in divideAndConquer.cpp; update main function to demonstrate usage

--- Algoritmiek/decreaseAndConquer.cpp
+++ Algoritmiek/decreaseAndConquer.cpp
@@ -27,12 +27,30 @@ int power(const int x, const int n)
     return x * power(x, n - 1);
 }
 
+int power2(const int x, const int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int half = power2(x, n / 2);
+
+    if (n % 2 == 0)
+    {
+        return half * half;
+    }
+    else
+    {
+        return x * half * half;
+    }
+}
+
 int main()
 {
     string s = "112211";
     int n = s.length();
 
-    cout << power(2, 4);
+    cout << power2(2, 4);
 
     return 0;
 }
--- Algoritmiek/divideAndConquer.cpp
+++ Algoritmiek/divideAndConquer.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
+int search(const int A[], const int left, const int right, const int x)
+{
+    if (left > right)
+    {
+        return -1;
+    }
+
+    int middle = (left + right) / 2;
+
+    if (A[middle] == x)
+        return middle;
+    else if (A[middle] > x)
+    {
+        return search(A, left, middle - 1, x);
+    }
+    else
+    {
+        return search(A, middle + 1, right, x);
+    }
+}
+
 int main()
 {
+    int A[] = {1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 99};
+    int n = sizeof(A) / sizeof(int);
+    cout << search(A, 0, n, 12);
     return 0;
 }
