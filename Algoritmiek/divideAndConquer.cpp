a8d36ddeadff3efd70f65a20f305310a14ee0562
Repsajsov <jaspervos12@outlook.com>
2026-01-15 21:28:47 +0100

    Refactor merge function to include detailed print statements for debugging; update main function to demonstrate usage with a larger array

--- Algoritmiek/divideAndConquer.cpp
+++ Algoritmiek/divideAndConquer.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+void print(const int A[], int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        cout << A[i] << ", ";
+    }
+    cout << endl;
+}
+
 int search(const int A[], const int left, const int right, const int x)
 {
     if (left > right)
@@ -32,11 +41,68 @@ bool twoZero(const int A[], const int left, const int right)
     return (twoZero(A, left, middle) || twoZero(A, middle, right)) ? true : false;
 }
 
+// void mergesort(const int A[], const int left, const int right)
+// {
+//     if (right - left == 1)
+//     {
+//         return;
+//     }
+// }
+
+void merge(int A[], const int left, const int middle, const int right)
+{
+    int leftSize = (middle - left);
+    int leftArray[leftSize];
+    int rightSize = right - middle;
+    int rightArray[rightSize];
+    for (int i = 0; i < leftSize; i++)
+    {
+        leftArray[i] = A[i];
+    }
+    for (int j = 0; j < rightSize; j++)
+    {
+        rightArray[j] = A[j + middle];
+    }
+    print(A, right);
+    print(leftArray, leftSize);
+    print(rightArray, rightSize);
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    while (i < leftSize || j < rightSize)
+    {
+        if (leftArray[i] >= rightArray[j])
+        {
+            A[k] = rightArray[j];
+            j++;
+        }
+        else
+        {
+            A[k] = leftArray[i];
+            i++;
+        }
+        k++;
+    }
+    for (; i < leftSize; i++)
+    {
+        A[k] = leftArray[i];
+        k++;
+    }
+    for (; j < rightSize; j++)
+    {
+        A[k] = rightArray[j];
+        k++;
+    }
+    print(A, right);
+}
+
 int main()
 {
-    int A[] = {1, 0, 0, 1};
+    int A[] = {1, 0, 0, 1, 2, 3, -1, -2, 3, 2403, -1234};
     int n = sizeof(A) / sizeof(int);
 
-    cout << twoZero(A, 0, n - 1);
+    merge(A, 0, n / 2, n);
+
     return 0;
 }
