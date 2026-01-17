ea3896336671b04833cd4c3f39389c785557c8a8
Repsajsov <jaspervos12@outlook.com>
2026-01-17 03:29:31 +0100

    Refactor dynamicProgramming.cpp; improve function formatting and rename longestSubArray to longestIncreasingSubArray for clarity

--- Algoritmiek/dynamicProgramming.cpp
+++ Algoritmiek/dynamicProgramming.cpp
@@ -3,41 +3,62 @@
 
 using namespace std;
 
-int fibonacciSlow(const int n) {
-    if (n == 0) return 0;
-    else if (n == 1) return 1;
+int fibonacciSlow(const int n)
+{
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
 
     return fibonacciSlow(n - 1) + fibonacciSlow(n - 2);
 }
 
-int fibonacci(const int n) {
+int fibonacci(const int &n)
+{
     int F[n + 1];
     F[0] = 0;
     F[1] = 1;
-    for (int i = 2; i <= n; i++) F[i] = F[i - 1] + F[i - 2];
+    for (int i = 2; i <= n; i++)
+        F[i] = F[i - 1] + F[i - 2];
     return F[n];
 }
 
-int longestSubArray(const int A[], const int& n) {
-    int max = INT_MIN;
-    for (int i = 0; i < n; i++) {
-        int count = 1;
-        int temp = A[i];
-        for (int j = i + 1; j < n; j++) {
-            if (temp < A[j]) {
-                count++;
-                temp = A[j];
-            }
-        }
-        if (count > max) max = count;
+int max(int const A[], int n)
+{
+    if (n == 0)
+    {
+        return A[0];
     }
-    return max;
+    int x = max(A, n - 1);
+    return (A[n] > x) ? A[n] : x;
 }
 
-int main() {
+int longestIncreasingSubArray(const int A[], const int &n)
+{
+    int L[n];
+    for (int i = 0; i < n; i++)
+        L[i] = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (A[j] < A[i])
+                L[i] = max(L[j] + 1, L[i]);
+        }
+    }
+    int maxLength = L[0];
+    for (int i = 1; i < n; i++)
+        if (L[i] > maxLength)
+            maxLength = L[i];
+    return maxLength;
+}
+
+int main()
+{
     int A[] = {3, 1, 4, 5, 2, 3, 6, 2, 8, 1, 7, 9, 9};
     int n = sizeof(A) / sizeof(int);
-    cout << longestSubArray(A, n);
+    cout << longestIncreasingSubArray(A, n);
 
     return 0;
 }
