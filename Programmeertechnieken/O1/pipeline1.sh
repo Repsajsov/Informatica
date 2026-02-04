6741a6ce819230cab34cfc4f99b73cff80c54933
Repsajsov <jaspervos12@outlook.com>
2026-02-04 20:31:05 +0100

    Refactor pipeline1.sh; add bzcat to all pipeline functions for consistent file handling

--- Programmeertechnieken/O1/pipeline1.sh
+++ Programmeertechnieken/O1/pipeline1.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
 pipeline1() {
-    tail -n +2 "$1" | cut -d ',' -f 1 | tr 'A-Za-z' 'X' | tr '0-9' '9' | sort -u
+    bzcat "$1" | tail -n +2 | cut -d ',' -f 1 | tr 'A-Za-z' 'X' | tr '0-9' '9' | sort -u
 }
 
 pipeline2() {
-    sed '1d' "$1" | cut -d ',' -f 3,4 | sort | uniq -c | sort -rn | head -n 10 | sed 's/^ *[0-9]* //'
+    bzcat "$1" | sed '1d' | cut -d ',' -f 3,4 | sort | uniq -c | sort -rn | head -n 10 | sed 's/^ *[0-9]* //'
 }
 
 pipeline3() {
-    awk -F ',' 'NR==1 {for(i=1; i<=NF; i++) if($i=="Taxi indicator") kolomTaxi=i; next} 
-                $kolomTaxi=="Ja" {voertuigen[$3 " " $4]++} 
-                END {for(voertuig in voertuigen) print voertuigen[voertuig],voertuig}' "$1" | sort -rn | head -n 10
+    bzcat "$1" | awk -F ',' '
+    NR==1 {
+        for(i=1; i<=NF; i++){ 
+            if($i=="Taxi indicator") {
+                kolomTaxi=i
+            }
+        }
+        next
+    } 
+    $kolomTaxi=="Ja" {
+        voertuigen[$3 " " $4]++
+    } 
+    END {
+        for(voertuig in voertuigen) {
+            print voertuigen[voertuig],voertuig 
+        }
+    }' | sort -rn | head -n 10
 }
 
 pipeline4() {
-    awk -F ',' '{merken[$3]++} END {for(merk in merken) if(merken[merk] >= 100) print merken[merk], merk}' "$1"
+    bzcat "$1" | awk -F ',' '
+    NR > 1 && $2=="Personenauto" {
+        merk = $3
+        split($21, leeftijd, "/")
+        if (leeftijd[3] != "") {
+            aantal[merk]++ 
+            somLeeftijd[merk]+=(2021 - leeftijd[3])
+        }
+    } 
+    END {
+    for(m in aantal) {
+        if(aantal[m] >= 100) { 
+            gemiddelde=int((somLeeftijd[m] / aantal[m]) + 0.5)
+            print gemiddelde, m
+            }
+        }
+    }' | sort -rn
 }
 
 pipeline4 "$1"
