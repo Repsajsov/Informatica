45ffb2cc4def1606db5c6bf17287c19d22ace6b8
Repsajsov <jaspervos12@outlook.com>
2026-02-04 16:31:49 +0100

    Refactor pipeline1.sh; remove unnecessary blank lines and adjust pipeline3 and pipeline4 functions for improved readability

--- Programmeertechnieken/O1/pipeline1.sh
+++ Programmeertechnieken/O1/pipeline1.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-
 pipeline1() {
     tail -n +2 "$1" | cut -d ',' -f 1 | tr 'A-Za-z' 'X' | tr '0-9' '9' | sort -u
 }
-
 
 pipeline2() {
     sed '1d' "$1" | cut -d ',' -f 3,4 | sort | uniq -c | sort -rn | head -n 10 | sed 's/^ *[0-9]* //'
 }
 
 pipeline3() {
-    
+    awk -F ',' 'NR==1 {for(i=1; i<=NF; i++) if($i=="Taxi indicator") kolomTaxi=i; next} 
+                $kolomTaxi=="Ja" {voertuigen[$3 " " $4]++} 
+                END {for(voertuig in voertuigen) print voertuigen[voertuig],voertuig}' "$1" | sort -rn | head -n 10
 }
 
-echo "Pipeline 1:"
-pipeline1 "$1"
-echo
-echo "Pipeline 2:"
-pipeline2 "$1"
+pipeline4() {
+    awk -F ',' '{merken[$3]++} END {for(merk in merken) if(merken[merk] >= 100) print merken[merk], merk}' "$1"
+}
+
+pipeline4 "$1"
